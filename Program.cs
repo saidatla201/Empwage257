feab54d77cd9d5edc6949af797d482657d3db9a6
saidatla201 <saidatla201@gmail.com>
2023-02-23 14:43:34 +0530

    Employee is present or absent

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+       public  static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Emp wage Calculation");
-            Empcheck.CheckEmployee();
-           
+            //UC1 Employee is present or Absent
+            const int EMP_PRESENT = 1;
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (randomInput == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+
+
         }
     }
 }
