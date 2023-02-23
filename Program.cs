f902944a6575339f44bdbaaf51a9896a38975eae
saidatla201 <saidatla201@gmail.com>
2023-02-23 14:51:14 +0530

    Calculate Empwage per hour

--- Program.cs
+++ Program.cs
@@ -4,20 +4,27 @@
     {
        public  static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Emp wage Calculation");
-            //UC1 Employee is present or Absent
+            Console.WriteLine("Welcome to Employee Wage Computation!");
             const int EMP_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            //UC-1 Check employee present or not
             Random random = new Random();
             int randomInput = random.Next(0, 2);
             if (randomInput == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
-
+            //UC-2 calculate Employee Wage
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage is :" + empWage);
 
 
         }
