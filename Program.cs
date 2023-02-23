21cdf5077e0d20dfcb37ac9dc903d367e9e817b4
saidatla201 <saidatla201@gmail.com>
2023-02-23 14:35:09 +0530

    Welcome to the Empwage Commutation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Empwage257
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Emp wage Calculation");
+            Empcheck.CheckEmployee();
+           
+        }
+    }
+}
